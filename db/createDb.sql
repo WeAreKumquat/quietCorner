8fd761970154124d118c9a6e4dff6879c90b1482
jsmichot <jsmichot@gmail.com>
2018-02-20 14:57:03 -0600

    (time/location-search) cleans

--- db/createDb.sql
+++ db/createDb.sql
@@ -1,13 +1,8 @@
 -- use this file by calling (from the root directory):
 -- mysql -u <your username> -p < db/createDb.sql
 
-
 -- uncomment following line to drop nola_events database:
 -- DROP DATABASE nola_events;
 
-
 -- create databse when this code is run:
 CREATE DATABASE nola_events;
-
-
-
