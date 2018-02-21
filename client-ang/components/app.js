0b8e156114a891a8205891e4c350bef8aadf2e85
jsmichot <jsmichot@gmail.com>
2018-02-21 08:56:27 -0600

    (time/location-search) functions set time/location

--- client-ang/components/app.js
+++ client-ang/components/app.js
@@ -8,7 +8,14 @@ angular.module('app')
       this.updateAppDate = function (selectedDate) {
         appMod.selectedDate = selectedDate;
       }.bind(appMod);
+      this.updateAppTime = function () {
+        console.dir(window.document.getElementById('time').value);
+        appMod.selectedTime = window.document.getElementById('time').value;
+      }.bind(appMod);
+      this.updateAppLocation = function (lat, long) {
+        console.log(lat, long);
+        appMod.selectedLocation = { latitude: lat, longitude: long };
+      }.bind(appMod);
     },
     templateUrl: '/templates/app.html',
   });
-
