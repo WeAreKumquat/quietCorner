8d21f74020b32bde057db9a089cb8d63825f6b3f
jsmichot <jsmichot@gmail.com>
2018-02-22 20:26:45 -0600

    (submit-throttle) sets values for display settings

--- client-ang/components/heatmapUI.js
+++ client-ang/components/heatmapUI.js
@@ -6,6 +6,7 @@ angular.module('app')
       updateAppLocation: '<',
       updateShowTraffic: '<',
       showTraffic: '<',
+      newMap: '<',
     },
     controller() {
       const heatUiMod = this;
@@ -17,30 +18,41 @@ angular.module('app')
       this.minDate.setDate(heatUiMod.dateNum - 1);
       this.maxDate = new Date();
       this.maxDate.setDate(heatUiMod.dateNum + 7);
+      this.location = 'New Orleans';
+      this.time = 'now';
+      this.displayDate = this.date.toString().slice(0, 16);
 
       this.selectDate = (newDate) => {
         const selectedDate = new Date(newDate);
         // update app's selectedDate:
         this.updateAppDate(selectedDate);
+        this.displayDate = selectedDate.toString().slice(0, 16);
       };
 
       this.getLocation = (address) => {
         if (address) {
           heatUiMod.updateAppLocation(null, null, address);
+          heatUiMod.location = address;
         } else if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition((data) => {
             console.dir(data);
             heatUiMod.updateAppLocation(data.coords.latitude, data.coords.longitude);
+            heatUiMod.location = 'your current location';
           });
         }
       };
 
       this.getTime = () => {
         heatUiMod.updateAppTime();
+        heatUiMod.time = window.document.getElementById('time').value;
       };
 
       this.toggleTraffic = () => {
         heatUiMod.updateShowTraffic();
+      };
+
+      this.submit = () => {
+        heatUiMod.newMap();
       };
     },
     templateUrl: '/templates/heatmapUI.html',
